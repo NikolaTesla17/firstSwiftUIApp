84cac3d065206080c35ebca13b7c12a2269104ec
Nayan Smuek <nayansmuek@nayan.local>
2020-06-19 14:23:59 -0700

    doesnt work but will

---
+++ Smuek Constant/Pods/Target Support Files/Pods-Smuek Constant-Smuek ConstantUITests/Pods-Smuek Constant-Smuek ConstantUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smuek_Constant_Smuek_ConstantUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Smuek_Constant_Smuek_ConstantUITests
+@end
---
+++ Smuek Constant/Pods/Target Support Files/Pods-Smuek ConstantTests/Pods-Smuek ConstantTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smuek_ConstantTests : NSObject
+@end
+@implementation PodsDummy_Pods_Smuek_ConstantTests
+@end
